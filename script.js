088113b202656bb660ac86ccf83b2f62b22c1546
Y. Emre BAKKAL <64362298+emrebakkal@users.noreply.github.com>
2022-08-17 20:43:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+/* This file only contains the animated background. (VantaJS) */
+
+VANTA.NET({
+    el: "#vanta",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 'white',
+    backgroundColor: '#2d3436',
+    showDots: false
+  })
